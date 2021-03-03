1ff63a84886d84eacff0d1f82fa500c58f3cd5a6
Cevat Sarıkaya <cvtsrky38@gmail.com>
2021-03-03 22:39:55 +0300

    Kamp 8. Gün Ekleme

---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context: Database tabloları ile proje classlarını bağlamak
+    public class NorthwindContext:DbContext
+    {
+        //Sql servera bağlanmak için
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true"); //Sql servera nasıl bağlanacağını belirtmemiz yeterli
+        }
+        //Aşağıda tablo eşitlemeleri yapılıyor. Bizim product sınıfımız products tablosuna gibi.
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
